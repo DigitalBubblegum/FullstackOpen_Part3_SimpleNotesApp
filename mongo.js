2b37947266f11da7babc0da618253895f1f0ab7c
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-10 19:08:57 +0530

    -small changes

--- mongo.js
+++ mongo.js
@@ -22,6 +22,7 @@ noteSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+   return returnedObject;
   },
 });
 
