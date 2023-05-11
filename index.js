1a5aa2dcd9664b3f47c715d829a2138cbdc3831a
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-11 18:46:26 +0530

    -followed example for connecting to backend database

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 const Note = mongoose.model('Note',noteSchema)
 app.use(cors());
 app.use(express.json())
