1f73d9f43f6820538c78819ea0f27e14d85c8d27
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-12 13:50:13 +0530

    -made small changes

--- models/note.js
+++ models/note.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 //DO NOT SAVE YOUR PASSWORD TO GITHUB
-const password = process.argv[2];
 const url = process.env.MONGODB_URI;
 console.log("connecting to", url);
 
