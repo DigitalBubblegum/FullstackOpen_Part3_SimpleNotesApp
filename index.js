af80f5594b409cda1275ae795b3ab509f67c7203
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-13 19:49:56 +0530

    -made small changes

--- index.js
+++ index.js
@@ -31,21 +31,21 @@ const generateId = () => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (body.content===undefined) {
     return response.status(400).json({
       error: "content missing",
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  })
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then(savedNote => {
+   response.json(savedNote);
+   console.log('saved note to db');
+  })
 });
 
 app.get('/',(request,response)=>{
@@ -60,13 +60,19 @@ app.get('/api/notes',(request,response) => {
   });
 })
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).send("<h1>404 not found</h1>").end();
-  }
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      response.status(500).end();
+    });
+  
 });
 app.delete('/api/notes/:id',(request,response)=>{
   const id = Number(request.params.id)
