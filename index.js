8a1ce9b8493e65197209da7c1cc2a17457df567a
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-15 11:06:57 +0530

    -followed example error handling into middleware

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require("cors");
+require('dotenv').config()
 const Note = require("./models/note");
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if(error.name === 'CastError'){
+    return response.status(400).send({error:'malformatted id'})
+  }
+  next(error)  
+}
+const unknownEndpoint = (request,response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
 app.use(cors());
 app.use(express.json())
 app.use(express.static("build"));
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+
+
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
@@ -62,7 +57,7 @@ app.get('/api/notes',(request,response) => {
   });
 })
 //get by ID
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response,next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -71,10 +66,7 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
   
 });
 //delete from DB
@@ -84,6 +76,8 @@ app.delete('/api/notes/:id',(request,response)=>{
   response.status(204).end()
   console.log('deleted');
 })
+app.use(unknownEndpoint)
+app.use(errorHandler)
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
