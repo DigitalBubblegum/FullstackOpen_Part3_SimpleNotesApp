a67a392d202d7d4d0170a2d97e44c42d6eda6581
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-05-17 18:30:08 +0530

    -followed example for linter

--- models/note.js
+++ models/note.js
@@ -1,32 +1,33 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+/* eslint-disable no-unused-vars */
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
 //DO NOT SAVE YOUR PASSWORD TO GITHUB
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
 
 mongoose
-  .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+	.connect(url)
+	.then((result) => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
 
 const noteSchema = new mongoose.Schema({
-  content: {
-    type: String,
-    minlength: 5,
-    required: true
-  },
-  important: Boolean,
-});
+	content: {
+		type: String,
+		minlength: 5,
+		required: true
+	},
+	important: Boolean,
+})
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-module.exports = mongoose.model("Note", noteSchema);
+noteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+	},
+})
+module.exports = mongoose.model('Note', noteSchema)
--- mongo.js
+++ mongo.js
@@ -1,45 +1,46 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+	console.log('give password as argument')
+	process.exit(1)
 }
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://ghoshaldiwakar:${password}@learningcluster0.yrx0i94.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const url = `mongodb+srv://ghoshaldiwakar:${password}@learningcluster0.yrx0i94.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
+	content: String,
+	important: Boolean,
 })
 
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-   return returnedObject;
-  },
-});
+noteSchema.set('toJSON', {
+	transform: (document, returnedObject) => {
+		returnedObject.id = returnedObject._id.toString()
+		delete returnedObject._id
+		delete returnedObject.__v
+		return returnedObject
+	},
+})
 
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is Easy',
-  important: true,
+	content: 'HTML is Easy',
+	important: true,
 })
 
+// eslint-disable-next-line no-unused-vars
 note.save().then(result => {
-  console.log('note saved!')
-  mongoose.connection.close()
+	console.log('note saved!')
+	mongoose.connection.close()
 })
 Note.find({}).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+	result.forEach((note) => {
+		console.log(note)
+	})
+	mongoose.connection.close()
+})
