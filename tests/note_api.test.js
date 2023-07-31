565870b54e16e3d334a5ca2332413cc2b91e1d1a
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-07-31 09:35:04 +0530

    promise all

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -7,11 +7,10 @@ const Note = require('../models/note')
 beforeEach(async () => {
 	await Note.deleteMany({})
 	console.log('cleared')
-	helper.initialNotes.forEach(async (note) => {
-		let noteObject = new Note(note)
-		await noteObject.save()
-		console.log('saved')
-	})
+	const noteObjects = helper.initialNotes
+		.map(note => new Note(note))
+	const promiseArray = noteObjects.map(note => note.save())
+	await Promise.all(promiseArray)
 	console.log('done')
 }, 100000)
 test('notes are returned as json',async () => {
