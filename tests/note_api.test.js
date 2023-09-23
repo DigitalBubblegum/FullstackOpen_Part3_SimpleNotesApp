cc19b314fa5accb6b35e12dca1061c675328af75
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-09-23 19:09:30 +0530

    -added test for user creation

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,13 +3,14 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
-
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
 const Note = require('../models/note')
 
 beforeEach(async () => {
 	await Note.deleteMany({})
 	await Note.insertMany(helper.initialNotes)
-})
+},100000)
 
 describe('when there is initially some notes saved', () => {
 	test('notes are returned as json', async () => {
@@ -110,6 +111,39 @@ describe('deletion of a note', () => {
 		expect(contents).not.toContain(noteToDelete.content)
 	})
 })
+describe('when there is initially one user in db', () => {
+	beforeEach(async () => {
+		await User.deleteMany({})
+
+		const passwordHash = await bcrypt.hash('sekret', 10)
+		const user = new User({ username: 'root', passwordHash })
+
+		await user.save()
+	},100000)
+
+	test('creation succeeds with a fresh username', async () => {
+		const usersAtStart = await helper.usersInDb()
+
+		const newUser = {
+			username: 'mluukkai',
+			name: 'Matti Luukkainen',
+			password: 'salainen',
+		}
+
+		await api
+			.post('/api/users')
+			.send(newUser)
+			.expect(201)
+			.expect('Content-Type', /application\/json/)
+
+		const usersAtEnd = await helper.usersInDb()
+		expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+		const usernames = usersAtEnd.map(u => u.username)
+		expect(usernames).toContain(newUser.username)
+	},100000)
+})
+
 afterAll(async () => {
 	await mongoose.connection.close()
 })
