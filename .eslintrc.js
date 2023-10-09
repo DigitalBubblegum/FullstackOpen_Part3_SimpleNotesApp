734d3ad100d2b42ac00b1400c77f6118490579d4
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-10-09 20:55:44 +0530

    -change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,23 @@
 module.exports = {
-	'env': {
-		'jest':true,
-		'node': true,
-		'commonjs': true,
-		'es2021': true
+	env: {
+		jest: true,
+		node: true,
+		commonjs: true,
+		es2021: true,
 	},
-	'extends': 'eslint:recommended',
-	'overrides': [
-	],
-	'parserOptions': {
-		'ecmaVersion': 'latest'
+	extends: 'eslint:recommended',
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
 	},
-	'rules': {
-		'eqeqeq':'error',
+	rules: {
+		eqeqeq: 'error',
 		'no-trailing-spaces': 'error',
-		'object-curly-spacing': [
-			'error', 'always'
-		],
-		'arrow-spacing': [
-			'error', { 'before': true, 'after': true }
-		],
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		]
-	}
+		'object-curly-spacing': ['error', 'always'],
+		'arrow-spacing': ['error', { before: true, after: true }],
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
 }
